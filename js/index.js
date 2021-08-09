421307d091bf4756c1331968706d38dfd1274a7e
tomasod123 <tmsdwyer50@gmail.com>
2021-08-09 14:10:33 +0800

    immediately before changing responsive mode of bottom half

---
+++ js/index.js
@@ -0,0 +1,24 @@
+
+// Smooth Scroll
+
+if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
+window.onmousewheel = document.onmousewheel = wheel;
+
+function wheel(event) {
+    var delta = 0;
+    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    else if (event.detail) delta = -event.detail / 3;
+
+    handle(delta);
+    if (event.preventDefault) event.preventDefault();
+    event.returnValue = false;
+}
+
+function handle(delta) {
+    var time = 1000;
+    var distance = 300;
+
+    $('html, body').stop().animate({
+        scrollTop: $(window).scrollTop() - (distance * delta)
+    }, time);
+}
